0d4eb9097c6fe20a8764ad115881cf0ee4a3c9d6
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2024-09-20 09:32:02 -0300

    feat: single binary

--- mix.exs
+++ mix.exs
@@ -8,21 +8,38 @@ defmodule HelloWorld.MixProject do
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: HelloWorld]
+      releases: releases()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {HelloWorld, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:do_it, "~> 0.6.1"}
+      {:do_it, "~> 0.6.1"},
+      {:burrito, "~> 1.1.0"}
+    ]
+  end
+
+  defp releases() do
+    [
+      hello_world: [
+        steps: [:assemble, &Burrito.wrap/1],
+        burrito: [
+          targets: [
+            linux: [os: :linux, cpu: :x86_64],
+            macos: [os: :darwin, cpu: :aarch64],
+            windows: [os: :windows, cpu: :x86_64]
+          ]
+        ]
+      ]
     ]
   end
 end
